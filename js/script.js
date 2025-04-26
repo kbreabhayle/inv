2c907709b10ebfa23d362a5b17cb10ffa48d6fc7
kbreabhayle <kbreabhayle132@gmail.com>
2025-04-26 01:25:42 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files (HTML, CSS, JS)
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Send message to everyone
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
